916d475e80bbd49b41d1c35462f277c7b54c47ad
Rohit Jakhar <rohitjakhar940@gmail.com>
2021-01-26 00:57:50 +0530

    Intial Project 3

--- app/src/main/java/com/todox/android/MainActivity.kt
+++ app/src/main/java/com/todox/android/MainActivity.kt
@@ -25,11 +25,15 @@ class MainActivity : AppCompatActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
+fun Greeting(
+        name: String
+) {
     Text(text = "Hello $name!")
 }
 
-@Preview(showBackground = true)
+@Preview(
+        showBackground = true
+)
 @Composable
 fun DefaultPreview() {
     TODOXTheme {
--- app/src/main/java/com/todox/android/ui/theme/Theme.kt
+++ app/src/main/java/com/todox/android/ui/theme/Theme.kt
@@ -28,7 +28,9 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun TODOXTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+fun TODOXTheme(
+        darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit
+) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
