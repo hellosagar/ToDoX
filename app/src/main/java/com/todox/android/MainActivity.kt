2eb566504d7ee56ea67011e97efde13a7dabdc51
pickachu404 <sagarkhgurana00786@gmail.com>
2021-01-25 19:05:38 +0530

    Hello World Jetpack Compose!

--- app/src/main/java/com/todox/android/MainActivity.kt
+++ app/src/main/java/com/todox/android/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
             TODOXTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Hello World!")
+                    Greeting("Hello World its Jetpack Compose!")
                 }
             }
         }
